9f99ff842cda5fbf645c8c4a2c3c315d3a797778
Opeyemi Adenola <yemi@Opeyemis-MacBook-Pro.local>
2020-01-15 09:59:48 +0000

    passed logout and access down for rendering of the lists on the nav bar

--- fomo_business_site/src/Components/NavBar.jsx
+++ fomo_business_site/src/Components/NavBar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-export default function NavBar() {
+export default function NavBar({ logOut, access}) {
   return (
     <div className="NavBar">
       <ul className="NavLinks">
         <Link to="/">
-        <li className="Link">HOME</li>
+        {!access && <li className="Link">HOME</li>}
         </Link>
         <Link to="/events">
           <li className="Link">EVENTS</li>
         </Link>
         <li className="Link">FEEDBACK</li>
-        <li className="Link">USER</li>
+        {!access && <li className="Link">USER</li>}
+        {access && <li className="Link" onClick={()=> logOut() }>Log Out</li>}
       </ul>
     </div>
   );
