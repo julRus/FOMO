a8ad3358ef2906f7597d7213072f6ddf02cb6d65
Opeyemi Adenola <yemi@Opeyemis-MacBook-Pro.local>
2020-01-15 10:00:53 +0000

    limited the routing of the navigation and path subjected by the accesskey s

--- fomo_business_site/src/App.js
+++ fomo_business_site/src/App.js
@@ -3,39 +3,63 @@ import { Router } from "@reach/router";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Events from "./Components/Events";
-import Home from "./Components/Home"
+import Home from "./Components/Home";
 import EventForm from "./Components/EventForm";
+import BusinessPage from "./Components/BusinessPage";
+import ErrorDisplay from "./Components/ErrorDisplay";
 
 class App extends React.Component {
   state = {
-    currentUser:"",
-    userId: null
+    currentUser: "",
+    userId: null,
+    businessName: "",
+    accessToken: null
+  };
+
+  componentDidMount() {
+    this.setState({
+      currentUser: "",
+      userId: null,
+      businessName: "",
+      accessToken: null || localStorage.token,
+      err: { msg: "please log in to view more" }
+    });
   }
- 
-  componentDidMount () {
-    this.setState({ currentUser: "", userId: null });
-  }
-  getUser = (username, id) => {
-    this.setState({currentUser: username, userId: id})
+  getUser = (username, businessName, id, accessToken) => {
+    this.setState({
+      currentUser: username,
+      userId: id,
+      businessName,
+      accessToken
+    });
+    localStorage.setItem('token', accessToken);
+  };
+
+  logOut = () => {
+    this.setState({accessToken: null})
+    localStorage.removeItem('token');
   }
 
-  
-  render () {
+  render() {
+    const { businessName, accessToken, err } = this.state;
     return (
-    <div className="App">
-      
-      <NavBar />
-      <Router>
-        <Home path="/" getUser={this.getUser} />
-        <Events path="/events"/>
-        <EventForm path="/events/newevent"/>
-        {/* <Feedback path="/Data" /> */}
-        {/* <User path="/User" />  */}
-      </Router>
-    </div>
-  );
+      <div className="App">
+        <NavBar access={accessToken} logOut={this.logOut}/>
+        <Router>
+          <Home path="/" getUser={this.getUser} />
+          <Events path={accessToken ? "/events" : "/"} />
+          <EventForm path={accessToken ? "/events/newevent" : "/"} />
+          <BusinessPage
+            business_name={businessName}
+            path={accessToken ? "business_account" : "/"}
+          />
+
+          {/* <Feedback path="/Data" /> */}
+          {/* <User path="/User" />  */}
+        </Router>
+      </div>
+    );
   }
-  
 }
 
 export default App;
